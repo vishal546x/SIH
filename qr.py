f3d43cd3f8b8886372123812ccf077a0af9496a0
vishal546x <vishalmoorthy8471@gmail.com>
2025-09-08 12:03:02 +0530

    qr generator added

--- qr.py
+++ qr.py
@@ -10,13 +10,13 @@ folder = "QRCodes"
 os.makedirs(folder, exist_ok=True)
 
 for _, s in students.iterrows():
-    data = f"{s['Name']},{s['Year']},{s['Dept']},{s['ID']}"
+    data = f"{s['roll_no']}"
     qr = qrcode.QRCode(version=1, box_size=10, border=4)
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
     
     # Save in folder
-    filename = os.path.join(folder, f"{s['ID']}_{s['Name']}.png")
+    filename = os.path.join(folder, f"{s['roll_no']}_{s['name']}.png")
     img.save(filename)
-    print(f"QR generated for {s['Name']} ✅")
+    print(f"QR generated for {s['name']} ✅")
